bd280833619e001b708b79a6b7b181b63afce802
HeyBroooo <mohitkumar4049a@gmail.com>
2024-01-24 23:31:15 +0530

    redesign2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-const {nextui} = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
@@ -6,7 +5,6 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     
   ],
   theme: {
