a1e8e3fed0d69be6eddddf1a0e26b91ceaf3c678
HeyBroooo <mohitkumar4049a@gmail.com>
2024-01-24 23:30:10 +0530

    redesign

--- app/page.js
+++ app/page.js
@@ -1,18 +1,42 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import TechStack from './components/TechStack'
-import Navbar from './components/Navbar'
-import Support from './components/Support'
+"use client";
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
 
 export default function Home() {
   return (
-   <div>
-    
-    <Navbar />
-    <Header />
-    <TechStack />
-    <Support />
-    <Footer />
-   </div>
-  )
+    <Navbar>
+      <NavbarBrand>
+        
+        <p className="font-bold text-inherit">ACME</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
 }
